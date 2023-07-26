6e668f2aec5003980aa8d4ed740dc190f028dbf1
bt12121212 <127172140+bt12121212@users.noreply.github.com>
2023-07-26 11:07:24 +0000

    1

--- api/download.js
+++ api/download.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = (req, res) => {
     const password = req.query.password;
     const correctPassword = 'aaaaaaaa';
+    const filePath = path.resolve(__dirname, '/tmp/result.json');
 
     if (password === correctPassword) {
-        res.download('/tmp/result.json');
+        // Check if file exists
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+            if (err) {
+                console.error(`File doesn't exist`);
+                res.status(404).send('文件不存在');
+            } else {
+                res.download(filePath);
+            }
+        });
     } else {
         res.status(403).send('密码错误');
     }
