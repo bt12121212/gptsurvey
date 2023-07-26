9967828f19f9eeaf0e26ce44ab306ac5d8bd6a40
bt12121212 <127172140+bt12121212@users.noreply.github.com>
2023-07-26 10:50:33 +0000

    password

--- api/download.js
+++ api/download.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-    if (req.query.password === '123456@a') {
+    if (req.query.password === 'aaaaaaaa') {
       res.download('/tmp/result.json');
     } else {
       res.status(403).send('密码错误');
