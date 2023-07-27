358c8180f076d2eeb6f602b2b778194f7bcb6c5b
bt12121212 <127172140+bt12121212@users.noreply.github.com>
2023-07-27 02:22:12 +0000

    new survey

--- api/submit.js
+++ api/submit.js
@@ -1,11 +1,12 @@
+const { json } = require('micro');
 let results = [];
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   if(req.method === 'POST') {
-    let result = req.body;
+    let result = await json(req);
     results.push(result);
     res.status(200).send("保存成功!");
   } else {
-    res.status(405).send("保存失败");
+    res.status(405).send("Method not allowed");
   }
 }
--- public/script.js
+++ public/script.js
@@ -1,12 +1,14 @@
 async function submitSurvey() {
     let forms = ['form1', 'form2', 'form3', 'form4', 'form5'];
     let result = {};
-  
-    forms.forEach(formId => {
+
+    forms.forEach((formId, index) => {
       let form = document.getElementById(formId);
-      result[formId] = form.q1.value;
+      let questionName = 'q' + (index + 1);
+      let input = form.elements[questionName];
+      result[formId] = input.value;
     });
-  
+
     let response = await fetch('/api/submit', {
       method: 'POST',
       headers: {
@@ -14,7 +16,7 @@ async function submitSurvey() {
       },
       body: JSON.stringify(result)
     });
-  
+
     if(response.ok) {
       alert('Result saved successfully!');
     } else {
