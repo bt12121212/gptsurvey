db39cd50f72b4232b308fe83ffc9ebf044162b84
bt12121212 <127172140+bt12121212@users.noreply.github.com>
2023-07-27 03:20:03 +0000

    new survey

--- public/script.js
+++ public/script.js
@@ -38,7 +38,7 @@ async function submitSurvey() {
     let password = prompt("请输入密码:");
     let response = await fetch("/api/download?password=" + password);
 
-    let tempcheck = prompt(response.ok,"::",esponse.ok.json());
+    let tempcheck = prompt(esponse.ok.json());
     if(response.ok) {
       let results = await response.json();
       console.log(results);
