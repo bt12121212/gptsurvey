5b122da23c27a20ec3ac583de76c31c6689ab51d
bt12121212 <127172140+bt12121212@users.noreply.github.com>
2023-07-27 03:25:17 +0000

    new survey

--- api/download.js
+++ api/download.js
@@ -3,6 +3,7 @@ module.exports = (req, res) => {
       let password = req.query.password;
       if(password === 'aaaaaaaa') {
         res.status(200).json(results);
+        alart("密码正确")
       } else {
         res.status(403).send("密码错误");
       }
