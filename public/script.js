7e55d72d924302255b2dcd967e20be6768a19dfd
bt12121212 <127172140+bt12121212@users.noreply.github.com>
2023-07-27 02:08:20 +0000

    new survey

---
+++ public/script.js
@@ -0,0 +1,23 @@
+async function submitSurvey() {
+    let forms = ['form1', 'form2', 'form3', 'form4', 'form5'];
+    let result = {};
+  
+    forms.forEach(formId => {
+      let form = document.getElementById(formId);
+      result[formId] = form.q1.value;
+    });
+  
+    let response = await fetch('/api/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(result)
+    });
+  
+    if(response.ok) {
+      alert('Result saved successfully!');
+    } else {
+      alert('An error occurred while saving the result.');
+    }
+  }
